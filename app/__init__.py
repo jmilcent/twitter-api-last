ac52dbac244fd90be277a3eb6d89449fb0132033
jmilcent <johan.milcent@gmail.com>
2018-11-29 16:15:18 +0100

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restplus import Api
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    @app.route('/hello')
+    def hello():
+        return "Goodbye World!"
+
+    from .apis.tweets import api as tweets
+    api = Api()
+    api.add_namespace(tweets)
+    api.init_app(app)
+
+    app.config['ERROR_404_HELP'] = False
+    return app
---
+++ app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from app import db
+
+class Tweet(db.Model):
+    __table__name = "tweets"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(280))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Tweet #{self.id}>"
